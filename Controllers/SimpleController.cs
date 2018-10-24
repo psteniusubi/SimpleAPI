14bdf7bd8d368e7e7b505747a7788b7dc32282e6
jeps <petteri.stenius@ubisecure.com>
2018-10-24 16:40:40 +0300

    update

---
+++ Controllers/SimpleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleAPI.OIDC;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("simple")]
+    [ApiController]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index([FromHeader(Name = "Authorization")] string authorization)
+        {
+            if (Client.TryValidateAuthorization(authorization, out var introspection))
+            {
+                var sub = (string)introspection["sub"];
+                var obj = new
+                {
+                    hello = sub
+                };
+                return new JsonResult(obj);
+            }
+            else
+            {
+                return Client.BearerToken(Client.API_CLIENT.ClientId);
+            }
+        }
+    }
+}
